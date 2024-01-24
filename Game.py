06e5a8f0d7aa61b40754dcab02a68b06c86e6c31
Eclairsombre <alexandre.thny@gmail.com>
2024-01-24 14:17:53 +0100

    Add evaluateFunction import and update play method

--- Game.py
+++ Game.py
@@ -1,60 +1,72 @@
+import evaluateFunction
+
+
 class ConnectFour:
-    def __init__(self) :
+    def __init__(self):
         self.board = [[' ' for _ in range(7)] for _ in range(6)]
         self.currentPlayer = 'X'
 
-    def printBoard(self) :
+    def printBoard(self):
         for row in self.board:
             print('|'.join(row))
-    
-    def isAPossibleMove(self,column) :
-        if(column > 6 or column < 0) : return False
-        elif(self.board[0][column]!=' ') : return False
-        else : return True
+
+    def isAPossibleMove(self, column):
+        if (column > 6 or column < 0):
+            return False
+        elif (self.board[0][column] != ' '):
+            return False
+        else:
+            return True
 
     def isWin(self, column):
         row = 0
-        while(self.board[row][column] == ' '):
+        while (self.board[row][column] == ' '):
             row += 1
         # Check vertical grid
-        if(row <= 2):
-            if(self.board[row][column] == self.board[row+1][column] == self.board[row+2][column] == self.board[row+3][column]):
+        if (row <= 2):
+            if (self.board[row][column] == self.board[row+1][column] == self.board[row+2][column] == self.board[row+3][column]):
                 return True
         # Check horizontal grid
         for i in range(max(0, column-3), min(3, column)):
-            if(self.board[row][i] == self.board[row][i+1] == self.board[row][i+2] == self.board[row][i+3]):
+            if (self.board[row][i] == self.board[row][i+1] == self.board[row][i+2] == self.board[row][i+3]):
                 return True
         # Check diagonal grid
         for i in range(4):
-            if(row+i-3 >= 0 and row+i <= 5 and column+i-3 >= 0 and column+i <= 6):
-                if(self.board[row+i-3][column+i-3] == self.board[row+i-2][column+i-2] == self.board[row+i-1][column+i-1] == self.board[row+i][column+i]):
+            if (row+i-3 >= 0 and row+i <= 5 and column+i-3 >= 0 and column+i <= 6):
+                if (self.board[row+i-3][column+i-3] == self.board[row+i-2][column+i-2] == self.board[row+i-1][column+i-1] == self.board[row+i][column+i]):
                     return True
-            if(row-i+3 <= 5 and row-i>=0 and column+i-3 >= 0 and column+i <= 6):
-                if(self.board[row-i+3][column+i-3] == self.board[row-i+2][column+i-2] == self.board[row-i+1][column+i-1] == self.board[row-i][column+i]):
+            if (row-i+3 <= 5 and row-i >= 0 and column+i-3 >= 0 and column+i <= 6):
+                if (self.board[row-i+3][column+i-3] == self.board[row-i+2][column+i-2] == self.board[row-i+1][column+i-1] == self.board[row-i][column+i]):
                     return True
         return False
-    
+
     def makeMove(self, column):
-        for row in range(5, -1, -1) :
-            if self.board[row][column] == ' ' :
+        for row in range(5, -1, -1):
+            if self.board[row][column] == ' ':
                 self.board[row][column] = self.currentPlayer
                 break
 
-    def switchPlayer(self) :
+    def switchPlayer(self):
         self.currentPlayer = 'O' if self.currentPlayer == 'X' else 'X'
-    
-    def copy(self) :
+
+    def copy(self):
         newGame = ConnectFour()
         newGame.board = [row[:] for row in self.board]
         newGame.currentPlayer = self.currentPlayer
         return newGame
 
+    def getRow(self, row):
+        return self.board[row]
 
-    def play(self) :
+    def getColumn(self, column):
+        return [row[column] for row in self.board]
+
+    def play(self):
         while True:
             self.printBoard()
-            column = int(input(f"Player {self.currentPlayer}, enter a column (0-6): "))
-            if(not self.isAPossibleMove(column)) :
+            column = int(
+                input(f"Player {self.currentPlayer}, enter a column (0-6): "))
+            if (not self.isAPossibleMove(column)):
                 print("Invalid move")
                 continue
             self.makeMove(column)
@@ -63,6 +75,12 @@ class ConnectFour:
                 self.printBoard()
                 break
             self.switchPlayer()
+            print(evaluateFunction.evaluateFunction(self))
 
-#game = ConnectFour()
-#game.play()
+# game = ConnectFour()
+# game.play()
+
+
+if __name__ == "__main__":
+    game = ConnectFour()
+    game.play()
---
+++ evaluateFunction.py
@@ -0,0 +1,137 @@
+
+def evaluateFunction(game):
+    result = 0
+    for i in range(6):
+        result += checkRow(game.getRow(i))
+    for i in range(7):
+        result += checkColumn(game.getColumn(i))
+    diagonals = getDiagonals(game)
+    result += checkDiagonals(diagonals)
+    return result
+
+
+def getDiagonals(game):
+    result = []
+
+    for i in range(3):
+        for j in range(4):
+            diagonal = []
+            inverseDiagonal = []
+            for k in range(4):
+                diagonal.append(game.board[i+k][j+k])
+                inverseDiagonal.append(game.board[i+k][6-j-k])
+            result.append(diagonal)
+            result.append(inverseDiagonal)
+    return result
+
+
+def checkRow(row):
+
+    result = 0
+    for i in range(4):
+        countX = 0
+        countO = 0
+        countSpace = 0
+        for j in range(4):
+            if (row[i+j] == 'X'):
+                countX += 1
+            elif (row[i+j] == 'O'):
+                countO += 1
+            else:
+                countSpace += 1
+        if (countX+countSpace == 4 and countSpace != 4):
+            match(countX):
+                case 4:
+                    result += 1000
+                case 3:
+                    result += 100
+                case 2:
+                    result += 5
+                case _:
+                    result += 1
+        elif (countO + countSpace == 4 and countSpace != 4):
+            match(countO):
+                case 4:
+                    result -= 1000
+                case 3:
+                    result -= 100
+                case 2:
+                    result -= 5
+                case _:
+                    result -= 1
+    return result
+
+
+def checkColumn(column):
+
+    result = 0
+    for i in range(3):
+        countX = 0
+        countO = 0
+        countSpace = 0
+        for j in range(4):
+            if (column[i+j] == 'X'):
+                countX += 1
+            elif (column[i+j] == 'O'):
+                countO += 1
+            else:
+                countSpace += 1
+        print(countX, countO, countSpace)
+        if (countX+countSpace == 4 and countSpace != 4):
+            match(countX):
+                case 4:
+                    result += 1000
+                case 3:
+                    result += 100
+                case 2:
+                    result += 5
+                case _:
+                    result += 1
+        elif (countO + countSpace == 4 and countSpace != 4):
+            match(countO):
+                case 4:
+                    result -= 1000
+                case 3:
+                    result -= 100
+                case 2:
+                    result -= 5
+                case _:
+                    result -= 1
+    return result
+
+
+def checkDiagonals(diagonals):
+
+    result = 0
+    for diagonal in diagonals:
+        countX = 0
+        countO = 0
+        countSpace = 0
+        for j in range(4):
+            if (diagonal[0+j] == 'X'):
+                countX += 1
+            elif (diagonal[0+j] == 'O'):
+                countO += 1
+            else:
+                countSpace += 1
+        if (countX+countSpace == 4 and countSpace != 4):
+            match(countX):
+                case 4:
+                    result += 1000
+                case 3:
+                    result += 100
+                case 2:
+                    result += 5
+                case _:
+                    result += 1
+        elif (countO + countSpace == 4 and countSpace != 4):
+            match(countO):
+                case 4:
+                    result -= 1000
+                case 3:
+                    result -= 100
+                case 2:
+                    result -= 5
+                case _:
+                    result -= 1
+    return result
