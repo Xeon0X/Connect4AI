5dc1e9d5bed645f839cba3ca70217811869faa15
Eclairsombre <alexandre.thny@gmail.com>
2024-03-28 19:18:15 +0100

    Fix stat

--- statistics_alphabeta.py
+++ statistics_alphabeta.py
@@ -31,20 +31,30 @@ def statAlphaBeta(depth: int):
     Args:
         depth (int): _description_
     """
-    depth_data = [{"depth": i+1, "nbWin": 0, "nbLoose": 0, "nbDraw":0, "win": [], "loose": [], "draw": []} for i in range(depth)]
-    
+    depth_data = [{"depth": i+1, "nbWin": 0, "nbLoose": 0, "nbDraw":0, "win": [], "loose": [], "draw": []} for i in range(0,depth)]
+    for i in range(0,depth):
+        print(f"Depth {i} : {depth_data[i]}")    
     def update_depth_data(result, depth1, depth2):
-        if result == depth1:
+        
+        result-=1
+        print(f"Result: {result}")
+        print(f"Depth IA1: {depth1}")
+        print(f"Depth IA2: {depth2}")
+       
+        if (result == depth1):
+            print("IA1 win")
             depth_data[depth1]["win"].append(depth2+1)
             depth_data[depth2]["loose"].append(depth1+1)
             depth_data[depth1]["nbWin"] += 1
             depth_data[depth2]["nbLoose"] += 1
-        elif result == depth2:
+        elif (result == depth2):
+            print("IA2 win")
             depth_data[depth1]["loose"].append(depth2+1)
             depth_data[depth2]["win"].append(depth1+1)
             depth_data[depth1]["nbLoose"] += 1
             depth_data[depth2]["nbWin"] += 1
         else:
+            print("Draw")
             depth_data[depth1]["draw"].append(depth2+1)
             depth_data[depth2]["draw"].append(depth1+1)
             depth_data[depth1]["nbDraw"] += 1
@@ -54,9 +64,7 @@ def statAlphaBeta(depth: int):
     for depth1 in range(depth-1):
         for depth2 in range(depth1+1,depth):
             print(f"---------------------------------")
-            print(f"Games started")
-            print(f"Depth IA1: {depth1+1}")
-            print(f"Depth IA2: {depth2+1}")
+            print(depth1, depth2)
             
             result = gameLoop(depth1+1, depth2+1)
             update_depth_data(result, depth1, depth2)
@@ -82,4 +90,4 @@ def statAlphaBeta(depth: int):
     print("Done!")
 
 if __name__ == "__main__":
-    statAlphaBeta(7)
+    statAlphaBeta(5)
